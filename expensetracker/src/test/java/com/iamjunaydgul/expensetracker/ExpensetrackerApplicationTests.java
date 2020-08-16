ad4778ecd86e838c034f17a9593e72e613eaf02e
iamjunaydgul <iamjunaydgul@gmail.com>
2020-08-16 00:11:46 -0700

    project setup completed :)

---
+++ expensetracker/src/test/java/com/iamjunaydgul/expensetracker/ExpensetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamjunaydgul.expensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
